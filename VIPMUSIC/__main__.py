8a70108f2384040a54e52ae0930fade6d0bf99ed
𝙉𝙖𝙮𝙧𝙖 ✗ 𝙍𝙤𝙗𝙤𝙩 <168265371+NayraXRobot@users.noreply.github.com>
2024-09-01 14:22:46 +0530

    Update __main__.py

--- VIPMUSIC/__main__.py
+++ VIPMUSIC/__main__.py
@@ -55,10 +55,10 @@ async def init():
     await userbot.start()
     await VIP.start()
     await VIP.decorators()
-    LOGGER("VIPMUSIC").info("VIPMUSIC Started Successfully")
+    LOGGER("VIPMUSIC").info("Telegram Started Successfully")
     await idle()
 
 
 if __name__ == "__main__":
     asyncio.get_event_loop_policy().get_event_loop().run_until_complete(init())
-    LOGGER("VIPMUSIC").info("Stopping VIPMUSIC! GoodBye")
+    LOGGER("VIPMUSIC").info("Stopping Telegram! GoodBye")
